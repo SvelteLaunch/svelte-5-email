beabb474bb12912ed48b7ce37224573056592fa6
Travis Mathis <travisdmathis@gmail.com>
2024-06-01 12:19:21 -0400

    initial commit

---
+++ src/lib/render.js
@@ -0,0 +1,32 @@
+import { convert } from 'html-to-text';
+import pretty from 'pretty';
+import { mount } from 'svelte';
+
+export const render = ({
+	template,
+	props,
+	options
+}) => {
+	const { html } = mount(template, {props: props})
+		template.render(props);
+	if (options?.plainText) {
+		return renderAsPlainText(html);
+	}
+	const doctype =
+		'<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
+	const markup = html;
+	const document = `${doctype}${markup}`;
+	if (options?.pretty) {
+		return pretty(document);
+	}
+	return document;
+};
+
+const renderAsPlainText = (markup) => {
+	return convert(markup, {
+		selectors: [
+			{ selector: 'img', format: 'skip' },
+			{ selector: '#__svelte-email-preview', format: 'skip' }
+		]
+	});
+};
